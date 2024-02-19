0215f717b669692e882923ec0605408ba5669336
August <160494361+bagge1@users.noreply.github.com>
2024-02-19 13:37:05 +0100

    Update pa_robot.py

--- pa_robot.py
+++ pa_robot.py
@@ -3,3 +3,7 @@ print("Hello world")
 x = [1,2,3,4,5,6,7,8,9]
 for i in x:
     print(f"Hello {i} Worlds")
+
+print("Super Earth needs YOU!",
+      "Sign up to fight for DEMOCRACY, LIBERTY and JUSTICE NOW!",
+      "JOIN THE HELLDIVERS")
