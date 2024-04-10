c015b308de1a7abdb5e896535462154434cc6753
Filip Widing <fiwi23@student.bth.se>
2024-04-10 11:55:42 +0200

    Fixed different heights(not fully done yet but it works) and started working on the shutdown button

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, TouchSensor, ColorSensor
 from pybricks.parameters import Port, Stop, Direction, Color, Button
 from pybricks.tools import wait
+import math
 
 
 ev3 = EV3Brick()
@@ -21,8 +22,9 @@ base_motor.control.limits(speed=60, acceleration=120)
 touchsensor = TouchSensor(Port.S1)
 
 colorsensor = ColorSensor(Port.S2)
+blocks_at_zone = [0,0,0,0]
+pickup_angles = [4,105,150,196]
 
-pickup_angles = [0,102,148,196]
 
 def closest_pudozone(angle):
      
@@ -34,6 +36,41 @@ def robot_calibrate():
     gripper_cal()
     arm_cal()
     base_cal()
+
+def blockselectscreen(nr):
+    ev3.screen.clear()
+    ev3.screen.draw_text(60,10,"Select the amount of blocks currently at the zone")
+    ev3.screen.draw_text(60,30,"UP")
+    ev3.screen.draw_text(60,50,str(nr))
+    ev3.screen.draw_text(60,70,"DOWN")
+
+def nrofblockssel():
+    ev3.screen.clear()
+    nrofblocks = 0
+    blockselectscreen(nrofblocks)
+    done = False
+    while not done:
+        pressed = ev3.buttons.pressed()
+        if Button.UP in pressed:
+            nrofblocks +=1
+            blockselectscreen(nrofblocks)
+            wait(500)
+            
+        if Button.DOWN in pressed:
+            nrofblocks -=1
+            blockselectscreen(nrofblocks)
+            wait(500)
+            
+        if Button.CENTER in pressed:
+            done = True
+            wait(1000)
+            ev3.screen.clear()
+            
+    return nrofblocks
+            
+
+
+    
 
 
 def manual_move():
@@ -57,7 +94,10 @@ def manual_move():
             base_motor.hold()
             
         if Button.UP in pressed and Button.UP not in was_pressed:
-            pickup_zones.append(closest_pudozone(base_motor.angle()))
+            curangle = closest_pudozone(base_motor.angle())
+            pickup_zones.append(curangle)
+            blocks_at_zone[pickup_angles.index(curangle)] = nrofblockssel()
+
 
         if Button.DOWN in pressed and Button.DOWN not in was_pressed:
             ev3.screen.clear()
@@ -72,21 +112,38 @@ def manual_move():
                 pressed = ev3.buttons.pressed()
                 if Button.UP in pressed and Button.UP not in was_pressed:
                     done = True
-                    putdown_zones["GREEN"] = closest_pudozone(base_motor.angle())
+                    curangle = closest_pudozone(base_motor.angle())
+                    putdown_zones["GREEN"] = curangle
+                    pickup_zones.append(curangle)
+                    wait(10)
+                    blocks_at_zone[pickup_angles.index(curangle)] = nrofblockssel()
                 elif Button.DOWN in pressed and Button.DOWN not in was_pressed:
                     done = True
-                    putdown_zones["RED"] = closest_pudozone(base_motor.angle())
+                    curangle = closest_pudozone(base_motor.angle())
+                    putdown_zones["RED"] = curangle
+                    pickup_zones.append(curangle)
+                    wait(10)
+                    blocks_at_zone[pickup_angles.index(curangle)] = nrofblockssel()
                 elif Button.LEFT in pressed and Button.LEFT not in was_pressed:
                     done = True
-                    putdown_zones["BLUE"] = closest_pudozone(base_motor.angle())
+                    curangle = closest_pudozone(base_motor.angle())
+                    putdown_zones["BLUE"] = curangle
+                    pickup_zones.append(curangle)
+                    wait(10)
+                    blocks_at_zone[pickup_angles.index(curangle)] = nrofblockssel()
                 elif Button.RIGHT in pressed and Button.RIGHT not in was_pressed:
                     done = True
-                    putdown_zones["YELLOW"] = closest_pudozone(base_motor.angle())
+                    curangle = closest_pudozone(base_motor.angle())
+                    putdown_zones["YELLOW"] = curangle
+                    pickup_zones.append(curangle)
+                    wait(10)
+                    blocks_at_zone[pickup_angles.index(curangle)] = nrofblockssel()
             ev3.screen.clear()
         
         if Button.CENTER in pressed and Button.CENTER not in was_pressed:
             check = False
     ev3.screen.clear()
+    wait(1000)
     return [pickup_zones, putdown_zones]
 
 def arm_move(position):
@@ -131,14 +188,6 @@ def rgb_detection():
     red = red_original / (green_original + 0.001)
     green = green_original / (blue_original + 0.001)
     blue = blue_original / (red_original + 0.001)
-    # wait(500)
-    # print('\n \n \n \n \n \n \n \n \n \n \n \n \n ')
-    # print("red")
-    # print(red)
-    # print("green")
-    # print(green)
-    # print("blue")
-    # print(blue)
     if red > 4:
         return "RED"
 
@@ -150,7 +199,10 @@ def rgb_detection():
 
     elif red < 1 and green < 0.8 and blue > 2:
         return "BLUE"
-
+    
+    elif red > 2 and green > 2 and blue > 2:
+        return "WHITE"
+    
     else:
         return 1
 
@@ -166,21 +218,28 @@ def most_frequent(List):
     return max(set(List), key = List.count)
 
 def block_pickup(angle):
-    base_move(angle)
+    anglecounter = base_motor.angle()
+    while anglecounter != angle:
+        base_motor.run_target(60,anglecounter)
+        anglecounter += 1
+        pressed = ev3.buttons.pressed()
+        if Button.CENTER in pressed:
+            shutdown_button()
     gripper_motor.run_target(50, -90)
-    elbow_motor.run_until_stalled(-20,Stop.HOLD, duty_limit=10)
+    arm_move(math.degrees(math.atan(blocks_at_zone[pickup_angles.index(angle)]*1.9/10))-30)
     gripper_motor.run_until_stalled(200, then=Stop.HOLD, duty_limit=75)
     elbow_motor.run_target(20, 0)
+    blocks_at_zone[pickup_angles.index(angle)]-=1
 
 
 def block_putdown(angle):
     base_move(angle)
-    elbow_motor.run_angle(200,-20,Stop.COAST)
+    # elbow_motor.run_angle(200,-20,Stop.COAST)
 
-
+    arm_move(math.degrees(math.atan(blocks_at_zone[pickup_angles.index(angle)]*1.9/10))-30)
     # elbow_motor.run_until_stalled(-20,Stop.COAST,duty_limit=10)
-    wait(10000)
-    elbow_motor.hold()
+    # elbow_motor.hold()
+    blocks_at_zone[pickup_angles.index(angle)]+=1
     gripper_motor.run_target(50, -90)
     elbow_motor.run_target(20,30)
 
@@ -192,11 +251,21 @@ def robot_func(zones):
     bd = block_detect()
     elbow_motor.run_target(20,30)
     if type(bd) == type(""):
-        block_putdown(putdown.get(bd))
+        if bd == "WHITE":
+            wait(1000)
+        else:
+            block_putdown(putdown.get(bd))
     else:
         wait(1000)
         block_putdown(base_motor.angle())
-    
+
+def shutdown_button():
+    while not Button.CENTER in pressed:
+        ev3.screen.draw_text(10,10,"Press the center button to resume")
+        pressed = ev3.buttons.pressed()
+        elbow_motor.hold()
+        gripper_motor.hold()
+        base_motor.hold()
 
 def main():
     robot_calibrate()
