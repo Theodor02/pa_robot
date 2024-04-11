d79397e524d7aa5be52cd7f668cf2ce3f3f47dc0
Filip Widing <fiwi23@student.bth.se>
2024-04-11 15:56:16 +0200

    Shutdown funktion and start with communication

--- main.py
+++ main.py
@@ -4,31 +4,31 @@ from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, TouchSensor, ColorSensor
 from pybricks.parameters import Port, Stop, Direction, Button
 from pybricks.tools import wait
+from pybricks.messaging import BluetoothMailboxServer, TextMailbox, BluetoothMailboxClient
 import math
 
-
+# Ev3 Motor stuff
 ev3 = EV3Brick()
-
 gripper_motor = Motor(Port.A)
-
 elbow_motor = Motor(Port.B, Direction.COUNTERCLOCKWISE, [8, 40])
-
 base_motor = Motor(Port.C, Direction.COUNTERCLOCKWISE, [12, 36])
-
-
 elbow_motor.control.limits(speed=60, acceleration=120)
 base_motor.control.limits(speed=60, acceleration=120)
-
 touchsensor = TouchSensor(Port.S1)
-
 colorsensor = ColorSensor(Port.S2)
-blocks_at_zone = [0,0,0,0]
-pickup_angles = [4,105,150,196]
 
+
+
+# Global Variables
+blocks_at_zone = [0, 0, 0, 0]
+pickup_angles = [4, 105, 150, 196]
+connection_state = True
+global schedule_wait
+is_multiplayer = False
 
 def closest_pudozone(angle):
 
-    return pickup_angles[min(range(len(pickup_angles)), key = lambda i: abs(pickup_angles[i]-angle))]
+    return pickup_angles[min(range(len(pickup_angles)), key=lambda i: abs(pickup_angles[i]-angle))]
 
 
 def robot_calibrate():
@@ -36,12 +36,14 @@ def robot_calibrate():
     arm_cal()
     base_cal()
 
+
 def blockselectscreen(nr):
     ev3.screen.clear()
-    ev3.screen.draw_text(60,10,"Select the amount of blocks currently at the zone")
-    ev3.screen.draw_text(60,30,"UP")
-    ev3.screen.draw_text(60,50,str(nr))
-    ev3.screen.draw_text(60,70,"DOWN")
+    ev3.screen.draw_text(60, 10, "Select the amount of blocks currently at the zone")
+    ev3.screen.draw_text(60, 30, "UP")
+    ev3.screen.draw_text(60, 50, str(nr))
+    ev3.screen.draw_text(60, 70, "DOWN")
+
 
 def nrofblockssel():
     ev3.screen.clear()
@@ -51,25 +53,18 @@ def nrofblockssel():
     while not done:
         pressed = ev3.buttons.pressed()
         if Button.UP in pressed:
-            nrofblocks +=1
+            nrofblocks += 1
             blockselectscreen(nrofblocks)
             wait(500)
-            
         if Button.DOWN in pressed:
-            nrofblocks -=1
+            nrofblocks -= 1
             blockselectscreen(nrofblocks)
             wait(500)
-            
         if Button.CENTER in pressed:
             done = True
             wait(1000)
             ev3.screen.clear()
-            
     return nrofblocks
-            
-
-
-    
 
 
 def manual_move():
@@ -84,10 +79,10 @@ def manual_move():
         ev3.screen.draw_text(10, 30, 'PICKUP = UP')
         ev3.screen.draw_text(10, 50, 'DROPOFF = DOWN')
         if Button.LEFT in pressed:
-            base_motor.run(20)
+            base_motor.run(60)
 
         elif Button.RIGHT in pressed and base_motor.angle() >= 0:
-            base_motor.run(-20)
+            base_motor.run(-60)
 
         else:
             base_motor.hold()
@@ -96,7 +91,6 @@ def manual_move():
             curangle = closest_pudozone(base_motor.angle())
             pickup_zones.append(curangle)
             blocks_at_zone[pickup_angles.index(curangle)] = nrofblockssel()
-
 
         if Button.DOWN in pressed and Button.DOWN not in was_pressed:
             ev3.screen.clear()
@@ -145,13 +139,58 @@ def manual_move():
     return [pickup_zones, putdown_zones]
 
 
-def arm_move(position):
-    elbow_motor.run_target(60, position)
-    return
+def schedule():
+    global schedule_wait
+    schedule_wait = 20000
+    ev3.screen.clear()
+    pressed = ev3.buttons.pressed()
+    while not Button.CENTER in pressed:
+        pressed = ev3.buttons.pressed()
+        ev3.screen.draw_text(10, 10, "Set wait time")
+        ev3.screen.draw_text(10, 30, "Center to confirm")
+        ev3.screen.draw_text(10, 50, "Wait time:" + str(schedule_wait/1000) + "s")
+        ev3.screen.draw_text(10, 70, "UP = +1s")
+        ev3.screen.draw_text(10, 90, "DOWN = -1s")
+        if Button.UP in pressed:
+            schedule_wait += 1000
+            wait(100)
+            ev3.screen.clear()
+        if Button.DOWN in pressed:
+            schedule_wait -= 1000
+            wait(100)
+            ev3.screen.clear()
+    ev3.screen.clear()
 
 
-def base_move(position):
-    base_motor.run_target(60, position)
+def arm_move(position, speed=60):
+    elbow_motor.run_target(speed, position, wait=False)
+    while elbow_motor.angle() != round(position):
+
+        ev3.screen.draw_text(10, 10, "Center button to abort")
+        pressed = ev3.buttons.pressed()
+        if Button.CENTER in pressed:
+            shutdown_button()
+            elbow_motor.run_target(speed, position, wait=False)
+    ev3.screen.clear()
+
+
+def base_move(position, speed=60):
+    base_motor.run_target(speed, position, wait=False)
+    while base_motor.angle() != position:
+        ev3.screen.draw_text(10, 10, "Center button to abort")
+        pressed = ev3.buttons.pressed()
+        if Button.CENTER in pressed:
+            shutdown_button()
+            base_motor.run_target(speed, position, wait=False)
+        if is_multiplayer:
+            if base_motor.angle() < 30:
+                send_occupied(mbox)
+            else:
+                send_unoccupied(mbox)
+            if recieve_occupied(mbox):
+                while recieve_occupied == True:
+                    base_motor.hold()
+    ev3.screen.clear()
 
 
 def arm_cal():
@@ -187,23 +226,24 @@ def rgb_detection():
     red = red_original / (green_original + 0.001)
     green = green_original / (blue_original + 0.001)
     blue = blue_original / (red_original + 0.001)
+    # wait(750)
+    # print("\n\n\n\n\n\n\n\n\n\n\n")
+    # print(red_original, green_original, blue_original)
+    # print(red, green, blue)
     if red > 4:
         return "RED"
 
-    elif red > 1.3 and green > 1.3 and blue < 1:
+    elif red > 1 and green > 1.3 and blue < 1:
         return "YELLOW"
 
     elif red < 1 and green > 0.8 and blue > 2:
         return "GREEN"
 
-    elif red < 1 and green < 0.8 and blue > 2:
+    elif red < 1 and green < 0.8 and blue > 2 and (red_original <=2 or blue_original > 30):
         return "BLUE"
-    
-    elif red > 2 and green > 2 and blue > 2:
-        return "WHITE"
-    
+
     else:
-        return 1
+        return "WHITE"
 
 
 def block_detect():
@@ -216,35 +256,25 @@ def block_detect():
 
 
 def most_frequent(List):
-    key = List.count
-    return max(set(List), key)
+    return max(set(List), key=List.count)
 
 
 def block_pickup(angle):
-    anglecounter = base_motor.angle()
-    while anglecounter != angle:
-        base_motor.run_target(60,anglecounter)
-        anglecounter += 1
-        pressed = ev3.buttons.pressed()
-        if Button.CENTER in pressed:
-            shutdown_button()
+    base_move(angle)
     gripper_motor.run_target(50, -90)
     arm_move(math.degrees(math.atan(blocks_at_zone[pickup_angles.index(angle)]*1.9/10))-30)
     gripper_motor.run_until_stalled(200, then=Stop.HOLD, duty_limit=75)
-    elbow_motor.run_target(20, 0)
-    blocks_at_zone[pickup_angles.index(angle)]-=1
+    arm_move(0, 20)
+    if blocks_at_zone[pickup_angles.index(angle)] > 0:
+        blocks_at_zone[pickup_angles.index(angle)] -= 1
 
 
 def block_putdown(angle):
     base_move(angle)
-    # elbow_motor.run_angle(200,-20,Stop.COAST)
-
-    arm_move(math.degrees(math.atan(blocks_at_zone[pickup_angles.index(angle)]*1.9/10))-30)
-    # elbow_motor.run_until_stalled(-20,Stop.COAST,duty_limit=10)
-    # elbow_motor.hold()
-    blocks_at_zone[pickup_angles.index(angle)]+=1
+    arm_move(math.degrees(math.atan(blocks_at_zone[pickup_angles.index(angle)]*1.9/10))-30, 30)
+    blocks_at_zone[pickup_angles.index(angle)] += 1
     gripper_motor.run_target(50, -90)
-    elbow_motor.run_target(20, 30)
+    arm_move(30, 20)
 
 
 def robot_func(zones):
@@ -252,40 +282,121 @@ def robot_func(zones):
     putdown = zones[1]
     block_pickup(pickup)
     bd = block_detect()
-    elbow_motor.run_target(20,30)
-    if type(bd) == type(""):
+    arm_move(30, 20)
+    if type(bd) is type(""):
         if bd == "WHITE":
-            wait(1000)
+            wait(schedule_wait)
         else:
             block_putdown(putdown.get(bd))
     else:
         wait(1000)
         block_putdown(base_motor.angle())
 
+
 def shutdown_button():
+    wait(300)
+    pressed = ev3.buttons.pressed()
+    ev3.screen.clear()
     while not Button.CENTER in pressed:
-        ev3.screen.draw_text(10,10,"Press the center button to resume")
+        ev3.screen.draw_text(10, 10, "Center button to resume")
+        ev3.screen.draw_text(10, 30, "UP to change schedule")
+        ev3.screen.draw_text(10, 50, "DOWN to change zones")
         pressed = ev3.buttons.pressed()
+        if Button.UP in pressed:
+            schedule()
+            ev3.screen.clear()
+            wait(300)
+        if Button.DOWN in pressed:
+            global zones
+            zones = manual_move()
+            ev3.screen.clear()
+            wait(300)
         elbow_motor.hold()
         gripper_motor.hold()
         base_motor.hold()
+    wait(300)
 
+# Robot messaging system
+
+
+# Robot messaging system
+def establish_connection(state):
+    if state is True:
+        server = BluetoothMailboxServer()
+        server.wait_for_connection(1)
+        print("Server established!")
+        mbox = TextMailbox("mbox", server)
+        while True:
+            msg = mbox.wait_new()
+            if msg == "ping":
+                mbox.send("pong")
+                return mbox
+    else:
+        client = BluetoothMailboxClient()
+        client.connect("EV3")
+        mbox = TextMailbox("mbox", client)
+        while True:
+            mbox.send("ping")
+            msg = mbox.wait_new()
+            if msg == "pong":
+                return mbox
+
+def recieve_occupied(mbox): # supposed to recieve true of location is occupied
+        location_msg = mbox.wait_new()
+        if location_msg == "False":
+            return False
+        else:
+            return True
+
+
+def send_occupied(mbox): # supposed to send true if location is occupied
+    msg = "True"
+    mbox.send(msg)
+
+def send_unoccupied(mbox): # supposed to send false if location is unoccupied
+    msg = "False"
+    mbox.send(msg)
+
+
+def server_or_client(connection_state):
+    while True:
+        ev3.screen.draw_text(10, 10, "UP for server")
+        ev3.screen.draw_text(10, 30, "DOWN for client")
+        pressed = ev3.buttons.pressed()
+        if Button.UP in pressed:
+            connection_state = True
+            return connection_state
+        if Button.DOWN in pressed:
+            connection_state = False
+            return connection_state
+
+
+def multiplayer():
+    while True:
+        ev3.screen.draw_text(10, 10, "UP for multirobot")
+        ev3.screen.draw_text(10, 30, "DOWN for single robot")
+        pressed = ev3.buttons.pressed()
+        if Button.UP in pressed:
+            return True
+        if Button.DOWN in pressed:
+            return False
+
+
+
+# Main function
 def main():
+    global zones
+    global mbox
+    is_multiplayer = multiplayer()
+    if is_multiplayer:
+        server_or_client(connection_state)
+        mbox = establish_connection(connection_state)
     robot_calibrate()
-    notes = [
-        "F#/3,A/3,C#/4",  # F#m chord
-        "A/3,C#/4,E/4",   # A major chord
-        "B/3,D#/4,F#/4",  # B major chord
-        "D/3,F#/3,A/3",   # D major chord
-        "C#/3,E/3,G#/3"   # C# minor chord
-    ]
-
-    # lyrics = "Ill take you to the candy shop  Ill let you lick the lollypop  Go ahead girl and dont you stop  Keep going till you hit the spot, WHOA!  (Ill take you to the candy shop)  (Boy one taste of what I got)  (Ill have you spending all you got)  (Keep going till you hit the spot, WHOA!)"
+    schedule()
     zones = manual_move()
-    ev3.speaker.play_notes(notes, tempo=117)
-    # ev3.speaker.say(lyrics)
-    for x in range(100):
+    while True:
         robot_func(zones)
+
 
 
 if __name__ == "__main__":
--- pa_robot.py
+++ pa_robot.py
@@ -9,3 +9,58 @@ print("Super Earth needs YOU!", "\n"
       "JOIN THE HELLDIVERS!")
 
 #Hello i am not.
+
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, TouchSensor, ColorSensor
+from pybricks.parameters import Port, Stop, Direction, Button
+from pybricks.tools import wait
+from pybricks.messaging import BluetoothMailboxServer, TextMailbox, BluetoothMailboxClient
+import math
+
+
+# Robot messaging system
+def establish_connection(state):
+    if state is True:
+        server = BluetoothMailboxServer()
+        server.wait_for_connection(1)
+        print("Server established!")
+        mbox = TextMailbox("mbox", server)
+        while True:
+            msg = mbox.wait_new()
+            if msg == "ping":
+                mbox.send("pong")
+                return mbox
+    else:
+        client = BluetoothMailboxClient()
+        client.connect("EV3")
+        mbox = TextMailbox("mbox", client)
+        while True:
+            mbox.send("ping")
+            msg = mbox.wait_new()
+            if msg == "pong":
+                return mbox
+
+def receive_location(mbox): # supposed to recieve true of location is occupied
+        location_msg = mbox.wait_new()
+        if location_msg == "False":
+            return False
+        else:
+            return True
+
+
+def send_occupied(mbox): # supposed to send true if location is occupied
+    msg = "True"
+    mbox.send(msg)
+    return
+
+
+def server_main(state):
+    safe_to_move = False
+
+    mbox = establish_connection(state)
+    if safe_to_move == False:
+        send_occupied(mbox)
+        safe_to_move = receive_location(mbox)
+    safe_to_move = receive_location(mbox)
+    
+    return safe_to_move
